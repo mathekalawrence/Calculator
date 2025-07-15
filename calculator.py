7548e9908c220743bdc8e2ed35851d780a2019b0
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-07-15 07:46:18 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+#A simple calculator in python!
+# asking the user for input: the first number using 'float()'
+num1 = float(input("Enter the first number: "))
+#Inputing the second float number value
+num2 = float(input("Enter the second number: "))
+
+#addding, subtracting, multiplying(product) and diving the two values
+sum_result=num2+num1
+difference_result = num1 - num2
+product_result = num1 * num2
+division_result = num2 / num1
+
+#Output of the operations
+print(f"Results of the two values:..")
+print(f"Sum: {sum_result}")
+print(f"Difference: {difference_result}")
+print(f"Product: {product_result}")
+print(f"Quotient/ Division: {division_result}")
